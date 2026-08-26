76550aaed71fb84ca0d8ee180bf3e287d07b54ba
Daemosthene <barmywizard123@gmail.com>
2026-08-26 12:16:03 -0600

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="card">
+        <h1>My React Application</h1>
+        <p>This application was created with React.</p>
+
+        <button onClick={() => alert('Hello from React!')}>
+          Click Me
+        </button>
+      </main>
     </div>
   );
 }
